58150a3bea5e1e7a744b0a3ce9e6e2052edbee00
Vanya Delova <happy.vanya.delova@gmail.com>
2018-10-20 20:51:32 +0200

    function updates

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,52 +1,69 @@
 // Write your JS here
-// Section 1
-
 
 let hero = {
-  name: "X-man",
-  heroic: true,
-  inventory: [],
-  health: 10,
-  weapon: {
-    type: "dagger",
-    damage: 2
-  }
+    name: "X-man",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "dagger",
+        damage: 2
+    }
 };
 
 function rest(player) {
-  player.health = 10;
-  return player;
+    player.health = 10;
+    return player;
 };
 
 function pickUpItem(player, item) {
-  player.inventory.push(item);
-  return player;
+    player.inventory.push(item);
+    return player;
 };
 
 function equipWeapon(player, index) {
-  let newWeapon = player.inventory[index];
-  player.weapon = newWeapon;
-  player.inventory.splice(newWeapon, 1);
-  return player;
+    let newWeapon = player.inventory[index];
+    player.weapon = newWeapon;
+    player.inventory.splice(newWeapon, 1);
+    return player;
 };
 
 
 var resting = document.getElementById('inn');
-resting.onclick = function(event) {
+resting.onclick = function (event) {
 
-    function rest(player){
-      player.health = 10
-      return player
+    function rest(player) {
+        player.health = 10
+        return player
     }
-  console.log(rest(hero));
+    console.log(rest(hero));
 }
 
 var pickUp = document.getElementById('dagger');
-pickUp.onclick = function(event) {
+pickUp.onclick = function (event) {
 
-    function pickUpItem(player, item){
-    player.inventory.push(item)
-    return player
+    function pickUpItem(player, item) {
+        player.inventory.push(item)
+        return player
     }
-  console.log(pickUpItem(hero, {type: "dagger", damage: 2}))
+    console.log(pickUpItem(hero, {
+        type: "dagger",
+        damage: 2
+    }))
 }
+
+
+hero.inventory = [{
+    type: "bomb",
+    damage: 8
+}, {
+    type: "pepperSpray",
+    damage: 5
+}];
+
+
+var equipment = document.getElementById('bag');
+equipment.addEventListener('click', function (event) {
+    var indexInput = prompt("What is your desired weapon?");
+
+});
