3b1c1c1c7c8a45c354edd5f031b1497a79657b31
Vanya Delova <happy.vanya.delova@gmail.com>
2018-10-20 16:57:18 +0200

    variables declaration

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,14 @@
 // Write your JS here
+// Section 1
+// Variables
+
+let hero = {
+  name: "X-man",
+  heroic: true,
+  inventory: [],
+  health: 10,
+  weapon: {
+    type: "dagger",
+    damage: 2
+  }
+};
