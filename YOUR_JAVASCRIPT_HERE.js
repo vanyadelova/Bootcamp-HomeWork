2264b642f4e14c3012a693c65268e9b4aed3617d
Vanya Delova <happy.vanya.delova@gmail.com>
2018-10-20 17:41:51 +0200

    functions updates

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -12,3 +12,20 @@ let hero = {
     damage: 2
   }
 };
+
+function rest(player) {
+  player.health = 10;
+  return player;
+};
+
+function pickUpItem(player, item) {
+  player.inventory.push(item);
+  return player;
+};
+
+function equipWeapon(player, index) {
+  let newWeapon = player.inventory[];
+  player.weapon = newWeapon;
+  player.inventory.splice(newWeapon, 1);
+  return player;
+};
