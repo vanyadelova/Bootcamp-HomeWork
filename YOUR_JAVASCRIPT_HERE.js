b180cb10ddac1101f73dc1b4dbfef3d489932aff
Vanya Delova <happy.vanya.delova@gmail.com>
2018-10-20 19:15:59 +0200

    functions update

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,6 +1,6 @@
 // Write your JS here
 // Section 1
-// Variables
+
 
 let hero = {
   name: "X-man",
@@ -24,8 +24,29 @@ function pickUpItem(player, item) {
 };
 
 function equipWeapon(player, index) {
-  let newWeapon = player.inventory[];
+  let newWeapon = player.inventory[index];
   player.weapon = newWeapon;
   player.inventory.splice(newWeapon, 1);
   return player;
 };
+
+
+var resting = document.getElementById('inn');
+resting.onclick = function(event) {
+
+    function rest(player){
+      player.health = 10
+      return player
+    }
+  console.log(rest(hero));
+}
+
+var pickUp = document.getElementById('dagger');
+pickUp.onclick = function(event) {
+
+    function pickUpItem(player, item){
+    player.inventory.push(item)
+    return player
+    }
+  console.log(pickUpItem(hero, {type: "dagger", damage: 2}))
+}
